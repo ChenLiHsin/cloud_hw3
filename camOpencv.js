f98db3dd1e6754cd2826f168582f213a2670b9d0
Chen Li-Hsin <41140021+ChenLiHsin@users.noreply.github.com>
2020-10-22 17:36:52 +0800

    Update camOpencv.js

--- camOpencv.js
+++ camOpencv.js
@@ -7,36 +7,63 @@ const disableWebcamButton = document.querySelector('#disableWebcamButton');
 const canvas = document.querySelector('#outputCanvas');
 const lowThresBox = document.querySelector('#lowThresInput') ;
 const highThresBox = document.querySelector('#highThresInput') ;
+// const submitBotton = document.querySelector('#submit') ;
 
 function onOpenCvReady() {
     document.querySelector('#status').innerHTML = 'opencv.js is ready.';
-    /* enable the button */
     m1.addEventListener('change', prepare) ;
     m2.addEventListener('change', prepare) ;
 }
 
-function prepare() {
-    if ( m1.checked ) {
-        if ( !disableWebcamButton2.disabled ) {
-            disableCam(disableWebcamButton2) ;
-            // disableWebcamButton2.disabled = true ;
+function isCanvasBlank(canvas) {
+    var blank = document.createElement('canvas') ;
+    blank.width = canvas.width ;
+    blank.height = canvas.height ;
+    return canvas.toDataURL() == blank.toDataURL() ;
+} 
+
+function prepare(event) {
+    if ( event.target.checked ) { 
+        if ( !isCanvasBlank(canvas) ) {  
+            video.srcObject.getTracks().forEach(track => {
+                track.stop();
+            })
+
+            /* clean up. some of these statements should be placed in processVid() */
+            video.srcObject = null;
+            video.removeEventListener('loadeddata', processVid);
+            const context = canvas.getContext('2d');
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            document.querySelector("#liveView").style.display = "none";
+  
         } // if()
-        lowThresBox.disabled = false ;
-        highThresBox.disabled = false ;
-        enableWebcamButton.disabled = false;
-        disableWebcamButton.disabled = true ;
-        enableWebcamButton2.disabled = true ;
+
+        if ( m1.checked ) {
+            lowThresBox.disabled = false ;
+            highThresBox.disabled = false ;
+            // submitBotton.disabled = false ;
+            enableWebcamButton2.disabled = true ;
+            disableWebcamButton2.disabled = true ;
+            lowThresBox.addEventListener('change', numCheck) ;
+            highThresBox.addEventListener('change', numCheck) ;
+            enableWebcamButton.disabled = false ;
+            enableWebcamButton.addEventListener('click', enableCam);
+
+            disableWebcamButton.addEventListener('click', disableCam);
+
+        } // if()
+        else if ( m2.checked ) {
+            enableWebcamButton2.addEventListener('click', enableCam) ;
+            disableWebcamButton2.addEventListener('click', disableCam);
+
+            enableWebcamButton2.disabled = false ;
+
+            enableWebcamButton.disabled = true ;
+            disableWebcamButton.disabled = true ;
+            lowThresBox.disabled = true ;
+            highThresBox.disabled = true ;
+        } // else if()
     } // if()
-    else if ( m2.checked ) {
-        if ( !disableWebcamButton.disabled ) {
-            disableCam(disableWebcamButton) ;
-            // disableWebcamButton.disabled = true ;
-        } // if()
-        enableWebcamButton2.disabled = false ;
-        lowThresBox.disabled = true ;
-        highThresBox.disabled = true ;
-        enableWebcamButton.disabled = true ;
-    } // else if()
 }
 
 /* Check if webcam access is supported. */
@@ -57,18 +84,14 @@ function getUserMediaSupported() {
      */
   
   if (getUserMediaSupported()) {
-    enableWebcamButton.addEventListener('click', enableCam);
-    enableWebcamButton2.addEventListener('click', enableCam) ;
-    disableWebcamButton.addEventListener('click', disableCam);
-    disableWebcamButton2.addEventListener('click', disableCam);
-    lowThresBox.addEventListener('change', numCheck) ;
-    highThresBox.addEventListener('change', numCheck) ;
+      m1.disabled = false ;
+      m2.disabled = false ;
   } else {
-    console.warn('getUserMedia() is not supported by your browser');
+      console.warn('getUserMedia() is not supported by your browser');
   }
 
   function enableCam(event) {
-    if ( !numCheck() ) return ;
+    if ( !numCheck ) return ;
     /* disable this button once clicked.*/
     event.target.disabled = true;
       
@@ -95,7 +118,6 @@ function getUserMediaSupported() {
   };
   
   function disableCam(event) {
-      console.log("here: ", event) ;
       event.target.disabled = true;
       if ( m1.checked ) enableWebcamButton.disabled = false;
       else enableWebcamButton2.disabled = false;
@@ -123,27 +145,42 @@ function getUserMediaSupported() {
     /* 8UC4 means 8-bit unsigned int, 4 channels */
     let frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
     cap.read(frame);
-    processFrame(frame);
+    if ( m1.checked ) processFrame(frame);
+    else if ( m2.checked ) processPencil(frame);
 }
 
+function checkAndEnableBotton() {
+    if ( isNum(lowThresBox.value) && isNum(highThresBox.value) ) {
+        enableWebcamButton.disabled = false ;
+        enableWebcamButton.addEventListener('click', enableCam);
+    } // if()
+} // decide()
+
+function isNum(str) {
+    var re = /^[0-9]+$/ ;
+    if ( !re.test(str) ) {
+        return false ;
+    } //if()
+    return true ;
+} // numCheck()
+
+
 function numCheck() {
-    let lowThresElement = document.getElementById("lowThresInput") ;
-    let highThresElement = document.getElementById("highThresInput") ;
     let low = true ;
     let high = true ;
     let result = false ;
 
     var re = /^[0-9]+$/ ;
 
-    if ( !re.test(lowThresElement.value)) {
+    if ( !re.test(lowThresBox.value)) {
         alert("Low Threshold Error: Only number is avaliable!!!") ;
-        lowThresElement.value = "" ;
+        lowThresBox.value = "" ;
         low = false ;
     } // if()
     
-    if ( !re.test(highThresElement.value)) {
+    if ( !re.test(highThresBox.value)) {
         alert("High Threshold Error: Only number is avaliable!!!") ;
-        highThresElement.value = "" ;
+        highThresBox.value = "" ;
         high = false ;
     } // if()
     
@@ -158,26 +195,52 @@ function numCheck() {
     } // else if()
     else {
         document.getElementById("err").innerHTML = "" ;
-        result = true ;
+        // result = true ;
     } // else()
-
-    return result ;
+    checkAndEnableBotton() ;
+    // return result ;
 } // numCheck
 
 function processFrame(src) {
     let dst = new cv.Mat();
-    let lowThresElement = document.getElementById("lowThresInput") ;
-    let highThresElement = document.getElementById("highThresInput") ;
 
     cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-    console.log("type: ",typeof(lowThres));
-    //console.log("low: ", lowThres);
-    //console.log("high: ",highThres) ;
-    cv.Canny(src, dst, parseInt(lowThresElement.value, 10), parseInt(highThresElement.value, 10));
+    cv.Canny(src, dst, parseInt(lowThresBox.value, 10), parseInt(highThresBox.value, 10));
     cv.imshow('outputCanvas', dst);
     src.delete();
     dst.delete();
 
+    /* Call this function again to keep processing when the browser is ready. */
+    window.requestAnimationFrame(processVid);  // 根據browser設定不斷呼叫(1/60秒呼叫一次)
+}
+
+function minus256(img) {
+    let img2 = new cv.Mat(240, 320, cv.CV_8UC1) ;
+    for ( var row = 0 ; row < 240 ; row ++ ) {
+      for ( var col = 0 ; col < 320 ; col ++ ) {
+            img2.ucharPtr( row, col)[0] = 255 - img.ucharPtr( row, col)[0] ;           
+      } // for()
+  } // for()
+  return img2 ;
+} // minus256()
+
+function processPencil(src) {
+    let grayDst = new cv.Mat();
+    let invertDst = new cv.Mat() ;
+    let blurDst = new cv.Mat() ;
+    let final = new cv.Mat() ;
+    let ksize = new cv.Size(5,5) ;
+    let anchor = new cv.Point(-1, -1);
+    cv.cvtColor(src, grayDst, cv.COLOR_RGBA2GRAY) ;
+    cv.bitwise_not(grayDst, invertDst) ;
+    cv.GaussianBlur(invertDst,blurDst,ksize,0,0,cv.BORDER_DEFAULT) ;
+    cv.divide( grayDst, minus256(blurDst), final, 256, -1 ) ;
+    cv.imshow('outputCanvas', final);
+
+    grayDst.delete() ;
+    invertDst.delete() ;
+    blurDst.delete() ;
+    // final.delete() ;
     /* Call this function again to keep processing when the browser is ready. */
     window.requestAnimationFrame(processVid);  // 根據browser設定不斷呼叫(1/60秒呼叫一次)
 }
